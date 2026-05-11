7af2113f34ec79cdb1ecd8c1d3163b30cd4c6fe9
天启之云 <fon1173@163.com>
2026-05-11 23:24:15 +0800

    更新时间

--- github-footer.js
+++ github-footer.js
@@ -1,7 +1,7 @@
 // 本地开发时：若未替换 __LAST_UPDATE__，则显示 "-"
 (function () {
     var el = document.querySelector('.github-footer-update');
-    if (el && el.textContent.indexOf('__LAST_UPDATE__') >= 0) {
+    if (el && el.textContent.indexOf('2026-05-11') >= 0) {
         el.textContent = '更新于 -';
     }
 })();
